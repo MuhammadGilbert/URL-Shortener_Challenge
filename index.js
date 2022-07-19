a323860034adc0a88baf2aabb700fb82a7a82a2d
Muhammad Gilbert <gilbertariel13@gmail.com>
2022-07-19 13:10:41 +0700

    fixed

--- index.js
+++ index.js
@@ -5,26 +5,25 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const mongo = require('mongodb')
-const dns = require('dns')
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const { Schema } = mongoose;
-const { model } = mongoose;
-
-const urlSchema = new Schema({
-  longurl: {type: String, required: true}
+let urlSchema = new mongoose.Schema({
+  original: {type: String, required: true},
+  short: {type: Number}
 });
 
-const Url = model("Url", urlSchema);
+let URL = mongoose.model("URL", urlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json())
+let responseObject = {}
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -37,30 +36,51 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.post('/api/shorturl', bodyParser.urlencoded({ extended: false }), async (req, res) => {
-  if (req.body.url.toString().match('/^https:/')){
-    const newurl = await new Url({
-      longurl: req.body.url
-    })
+app.post('/api/shorturl', bodyParser.urlencoded({ extended: false }),(req, res) => {
+  let inputUrl = req.body['url']
 
-  const shorturl = await newurl.save();
-  res.json({original_url: req.body.url, short_url: shorturl._id});
-  } else {
-    res.json({error: "invalid url"});
+  let urlRegex = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi)
+
+  if (!inputUrl.match(urlRegex)){
+    res.json({error: 'invalidURL'})
+    return
   }
+    
+  responseObject['original_url'] = inputUrl
+
+  let inputShort = 1
+  URL.findOne({})
+        .sort({ short: 'desc' })
+        .exec((error, result) => {
+          if(!error && result != undefined){
+            inputShort = result.short + 1
+          }
+          if(!error){
+            URL.findOneAndUpdate(
+              {original: inputUrl},
+              {original: inputUrl, short: inputShort},
+              {new: true, upsert: true},
+              (error, savedUrl) => {
+                if(!error){
+                  responseObject['short_url'] = savedUrl.short;
+                  res.json(responseObject);
+                }
+              }
+            )
+          }
+        })
 })
+app.get('/api/shorturl/:input', (req, res) => {
+  let input = req.params.input
 
-app.get('/api/shorturl/short:', (req, res) => {
-  function getShortUrl(){ 
-    Url.findById(req.params.short).limit(1).exec((err, data) => {
-      if(err) return console.log(err);
-      console.log(data);
-      res.redirect(data.longurl)
-    });
-  }
-  getShortUrl();
-});
-
+  URL.findOne({short: input}, (error, result) => {
+    if(!error && result != undefined){
+      res.redirect(result.original)
+    }else{
+      res.json('URL not found')
+    }
+  })
+})
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
